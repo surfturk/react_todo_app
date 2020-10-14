36057a8cb89f2e593b878231700d1dc0bf433d51
github username <github email address>
2020-10-14 08:37:33 -0500

    added delete function

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import Todos from './todos'
+
+
+class App extends Component {
+  state = {
+    todos: [
+      {id: 1, content: 'buy some milk'},
+      {id:2, content: 'play guitar'}
+    ]
+  }
+  deleteTodo = (id) => {
+    const todos = this.state.todos.filter(todo => {
+      return todo.id !== id
+    });
+    this.setState({
+      todos: todos
+    })
+  }
+
+  render() {
+  return (
+    <div className="todo-pp container">
+      <h1 className="center blue-text">Todos's</h1>
+      <Todos todos={this.state.todos} deleteTodo={this.deleteTodo}/>
+      
+    </div>
+  );
+ }
+}
+
+export default App;
