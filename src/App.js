6c0d062581f635a7025ff8299fcbb92f1271c024
github username <github email address>
2020-10-14 09:05:48 -0500

    finished adding add todo function

--- src/App.js
+++ src/App.js
@@ -20,7 +20,11 @@ class App extends Component {
   }
 
   addTodo = (todo) => {
-
+     todo.id = Math.random();
+     let todos = [...this.state.todos, todo]
+     this.setState({
+       todos: todos
+     })
   }
 
   render() {
